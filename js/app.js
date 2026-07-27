ec9e8f52f6c5a2ae9e597e7d7afe2a88f49a8411
bharata65 <bharataa65@gmail.com>
2026-07-27 13:16:18 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,49 @@
+import "./firebase-config.js";
+
+const app=document.getElementById("app");
+
+app.innerHTML=`
+
+<header class="header">
+
+<div class="logo">
+
+SKILL GAME
+
+</div>
+
+<div class="balance">
+
+₹0.00
+
+</div>
+
+</header>
+
+<div class="banner"></div>
+
+<section class="section">
+
+<div class="card">
+
+<h2>Daily Puzzle Contest</h2>
+
+<br>
+
+<p>Entry ₹10</p>
+
+<p>Prize Pool ₹100</p>
+
+<br>
+
+<button class="join">
+
+JOIN NOW
+
+</button>
+
+</div>
+
+</section>
+
+`;
